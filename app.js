57f4cb21925abcbd7240248e1e4560bfafbe9972
İrem Çakır <95527331+iremcakirrr@users.noreply.github.com>
2021-12-29 22:37:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+//elementlerı secme
+
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+const github = new Github();
+const ui = new UI();
+eventListeners();
+
+function eventListeners(){
+
+    githubForm.addEventListener("submit",getData);
+    clearLastUsers.addEventListener("click",clearAllSearched);
+    document.addEventListener("DOMContentLoaded",getAllSearched);
+
+}
+
+function getData(e){
+    let username = nameInput.value.trim();
+
+    if(username === ""){
+
+        alert("Please enter a username");
+    }
+    else{
+        github.getGithubData(username)
+        .then(response => {
+            if(response.user.message === "Not Found"){
+                //hata
+                ui.showError("kullanıcı bulunamadı");
+            }
+            else{
+                ui.addSearchedUserToUI(username);
+
+
+
+                Storage.addSearchedUserToStorage(username);
+                ui.showUserInfo(response.user);
+                ui.showRepoInfo(response.repo);
+            }
+        })
+        .catch(err => ui.showError(err));
+
+    }
+    ui.clearInput();
+    e.preventDefault();
+}
+
+function clearAllSearched(){
+    //tum arananları temızle
+
+    if(confirm("Emin misiniz ? ")){
+        Storage.clearAllSearchedUsersFromStorage(); //storagedan temızle
+        ui.clearAllSearchedFromUI();
+    }
+
+}
+function getAllSearched(){
+    //arananları storagedan al ve ui ye ekle 
+
+    let users = Storage.getSearchedUsersFromStorage();
+
+    let result = "";
+
+    users.forEach(user => {
+        //<li class="list-group-item">asdaskdjkasjkşdjşasjd</li>
+
+        result += `<li class="list-group-item">${user}</li>`;
+
+
+    });
+
+    lastUsers.innerHTML = result;
+
+
+
+}
